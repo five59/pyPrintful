8559399e037015ef1e62d3f8e76df8026497f8b9
Andrew Marconi <andrew@559labs.com>
2017-03-07 14:59:38 -0500

    Added dummy classes
    Added dummy classes whilst setting up documentation.

---
+++ pyPrintful/__init__.py
@@ -0,0 +1,12 @@
+class pyPrintful():
+    """
+    The main pyPrintful class.
+    """
+    pass
+
+
+class pyPrintfulException(Exception):
+    """
+    An exception derrived from the Exception class.
+    """
+    pass
