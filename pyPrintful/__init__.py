affb3be7d737890430ea823e85bbf041724769d4
Andrew Marconi <andrew@559labs.com>
2017-03-08 10:54:22 -0500

    Now correctly use a single key rather than ck and cs
    Now correctly use a single key rather than ck and cs
    Upped to version 1.0.0a5.

--- pyPrintful/__init__.py
+++ pyPrintful/__init__.py
@@ -16,7 +16,7 @@ class pyPrintful:
     :returns: A stateful object with an authenticated connection.
     """
 
-    VERSION = "1.0.0a4"
+    VERSION = "1.0.0a5"
 
     _store = {
         'base_url': 'https://api.printful.com/',
